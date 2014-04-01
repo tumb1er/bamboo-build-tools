b59ff6c9c950b45e57ef751ba0e3839d52d677de
Sergey Tikhonov <zimbler@gmail.com>
2014-04-01 15:44:09 +0400

    fix coverage 2to3 issue - 3.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-build-tools',
-    version='3.1.2',
+    version='3.1.3',
     packages=['bamboo'],
     url='http://rutube.ru',
     license='Beer Licence',
