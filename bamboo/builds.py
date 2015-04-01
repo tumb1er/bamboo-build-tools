a0d9acb55e9edced74fc08b79658fe6e4dbc2c56
Sergey Tikhonov <zimbler@gmail.com>
2015-04-01 16:55:43 +0300

    fix bamboo api client init and bump version

--- bamboo/builds.py
+++ bamboo/builds.py
@@ -1,20 +1,43 @@
 # coding: utf-8
 
 # $Id: $
+from copy import copy
 import json
 from jira.client import JIRA, translate_resource_args
 from jira.utils import raise_on_error
 from bamboo.helpers import parse_config
 
 
+class Bamboo(JIRA):
+
+    def __init__(self, server=None, options=None, basic_auth=None, oauth=None,
+                 validate=None, async=False, logging=True, max_retries=3):
+        headers = copy(JIRA.DEFAULT_OPTIONS['headers'])
+        headers.update({'Accept': 'application/json'})
+        options = options or {}
+        options.setdefault('headers', {})
+        options['headers'].update(headers)
+        super(Bamboo, self).__init__(server, options, basic_auth, oauth,
+                                     validate, async, logging, max_retries)
+
+
+
+
+    def server_info(self):
+        info = self._get_json('info')
+        info['versionNumbers'] = info['version'].split('.')
+        return info
+
+
 class Builds(object):
     def __init__(self, configfile='bamboo.cfg'):
-        self.jira_user = 'bamboo'
-        self.jira_password = 'bamboo'
+        self.jira_user = 'stikhonov'
+        self.jira_password = '3P2Qsd4f'
         self.server_name = 'http://bamboo.rutube.ru:8085'
         parse_config(self, configfile)
-        self.jira = JIRA({'server': self.server_name, 'rest_api_version': '1.0'},
-                         basic_auth=(self.jira_user, self.jira_password))
+        self.jira = Bamboo(server=self.server_name,
+                           options={'rest_api_version': '1.0'},
+                           basic_auth=(self.jira_user, self.jira_password))
 
     @translate_resource_args
     def add_comment(self, build, body):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-build-tools',
-    version='3.2.4',
+    version='3.2.6',
     packages=['bamboo'],
     url='http://rutube.ru',
     license='Beer Licence',
