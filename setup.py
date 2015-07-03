646e51d209b6d11789184fdad107ff607565ef06
tumb1er <zimbler@gmail.com>
2015-07-03 10:29:28 +0300

    Bump version up to 3.3 - python3 support merged

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-build-tools',
-    version='3.2.7',
+    version='3.3.0',
     packages=['bamboo'],
     url='http://rutube.ru',
     license='Beer Licence',
