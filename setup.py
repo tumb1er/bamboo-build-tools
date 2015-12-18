ac9b38b7536bb1807b837509465733175c05ecc2
tumb1er <zimbler@gmail.com>
2015-12-18 12:29:09 +0300

    Bump version to 5.0.0 - gitflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-build-tools',
-    version='4.0.1',
+    version='5.0.0',
     packages=['bamboo'],
     url='http://rutube.ru',
     license='Beer Licence',
