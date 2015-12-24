56d330b830738daf83206496c91bd3c2400f1e84
tumb1er <zimbler@gmail.com>
2015-12-24 18:47:21 +0300

    Bump version to 5.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-build-tools',
-    version='5.0.0',
+    version='5.1.0',
     packages=['bamboo'],
     url='http://rutube.ru',
     license='Beer Licence',
